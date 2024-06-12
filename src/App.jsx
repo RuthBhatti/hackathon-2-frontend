4a759afe2de79c085174c0e0ce062fc279815f45
Danielle Bruce <you@example.com>
2024-06-12 14:28:17 -0400

    Fixes axios request

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,25 @@ import Board from './components/Board/Board'
 import Question from './components/Question/Question'
 
 function App() {
-  const [count, setCount, ] = useState(0)
+  const [trivia, setTrivia] = useState([])
+
+  const getTriviaData = async () => {
+      try {
+          const res = await axios(`http://localhost:8080/questions`)
+          setTrivia(res.data)
+      } catch (err) {
+          console.log(err)
+      }
+  }
+
+  useEffect(() => {
+      getTriviaData()
+  }, [])
 
   return (
     <>
-      <Board></Board>
-      <Question></Question>
+      <Board trivia={trivia}></Board>
+      <Question trivia={trivia}></Question>
     </>
   )
 }
