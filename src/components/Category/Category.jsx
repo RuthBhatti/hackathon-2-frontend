4c86b0ec77ee3d3c3868efb47e2ebeda765e2ba8
Danielle Bruce <you@example.com>
2024-06-12 17:48:33 -0400

    adds styling

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,16 +5,17 @@ import { Link } from "react-router-dom";
 const Category = ({ categoryData, setQuestion }) => {
 return (
 <>
-    <section>
+    <section className="category">
     <header>{categoryData.topic}</header>
     {categoryData.questions.map((question) => {
         return (
         <Link
             to={`/${categoryData.uuid}+${question.points}`}
             key={question.points}
+            className="price-button__text"
         >
             <article className="price-button">
-            <h2>{question.points}</h2>
+                <h2>{question.points}</h2>
             </article>
         </Link>
         );
