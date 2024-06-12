fe5da22575b7e2135b9e543a9cc8b354d0b5985d
Danielle Bruce <you@example.com>
2024-06-12 15:20:02 -0400

    question styling

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import './Question.scss'
 
 const Question = ({trivia}) => {
 
     return (
         <div className="question">
-            <h2>Question</h2>
-            <ul>
-                <li>Answer A</li>
-                <li>Answer B</li>
-                <li>Answer C</li>
-                <li>Answer D</li>
+            <h2 className='question__header'>Question</h2>
+            <p className='question__question'>What is my name?</p>
+            <ul className='question__answers'>
+                <li className='question__answer'>Answer A</li>
+                <li className='question__answer'>Answer B</li>
+                <li className='question__answer'>Answer C</li>
+                <li className='question__answer'>Answer D</li>
             </ul>
         </div>
     );
