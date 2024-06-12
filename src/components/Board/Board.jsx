3df19dcaa4c8b8f396f9a8a0c99d86d45f8d74db
Danielle Bruce <you@example.com>
2024-06-12 13:59:34 -0400

    adds API call and begins to pass data down to components

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './Board.scss'
 import Category from '../Category/Category';
+import axios from 'axios';
 
 
 const Board = () => {
+    const [trivia, setTrivia] = useState([])
+
+    const getTriviaData = async () => {
+        try {
+            const res = await axios(`http://localhost:8080/questions`)
+            setTrivia(res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getTriviaData()
+    }, [])
+
     return (
         <>
             <header>
                 <h1>NAME</h1>
             </header>
             <div className='price-box'>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
+                {trivia.map(categoryData => {
+                    return (<Category 
+                    categoryData = {categoryData}
+                    key={categoryData.topic}
+                    />)
+                }
+                )}
             </div>
         </>
     );
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import './Category.scss'
 
-const Category = ({ data }) => {
+const Category = ({ categoryData }) => {
 
     return (
         <>
 
             <section>
-                <header>categoryName</header>
-                <article className='price-button'>
-                    <h2>$200.00</h2>
-                </article>
-                <article className='price-button'>
-                    <h2>$400.00</h2>
-                </article>
-                <article className='price-button'>
-                    <h2>$600.00</h2>
-                </article>
-                <article className='price-button'>
-                    <h2>$800.00</h2>
-                </article>
-                <article className='price-button'>
-                    <h2>$1000.00</h2>
-                </article>
+                <header>{categoryData.topic}</header>
+                {categoryData.questions.map(question => {
+                    return (
+                    <article className='price-button' key={question.points}>
+                        <h2>{question.points}</h2>
+                    </article>
+                    )
+                })}
             </section>
         </>
     )
